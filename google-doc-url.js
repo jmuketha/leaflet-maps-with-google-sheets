b5cf0b0b1d375c51e1a7182fe025750f0dd14d49
jmuketha <51195319+jmuketha@users.noreply.github.com>
2019-05-31 13:09:48 +0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1TxN8cYGYiRRwOhm9_Zo8WThBQO2c3scrUaPDVFigdJk/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1TxN8cYGYiRRwOhm9_Zo8WThBQO2c3scrUaPDVFigdJk/edit';
